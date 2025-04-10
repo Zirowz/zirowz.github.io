bc08a41ccccc31e3846781d1a80dbfdc6ffcf68d
Zirowz <123315998+Zirowz@users.noreply.github.com>
2025-04-10 20:38:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+async function getPlayerCount() {
+  try {
+    const response = await fetch("https://api.mcstatus.io/v2/status/bedrock/play.arisia.fr:19132");
+    const data = await response.json();
+    const nbJoueurs = data.players.online;
+    document.getElementById("nb-joueurs").textContent = nbJoueurs;
+  } catch (error) {
+    console.error("Erreur en récupérant les joueurs :", error);
+    document.getElementById("nb-joueurs").textContent = "Erreur";
+  }
+}
+
+getPlayerCount();
+
+setInterval(getPlayerCount, 30000);
